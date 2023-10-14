36d386910cf68a3f8e7cfb72334406e054941080
Leo Curdi <ldcurdi@icloud.com>
2023-10-14 13:07:19 -0700

    homepage

--- test2/src/App.js
+++ test2/src/App.js
@@ -32,15 +32,17 @@ function App() {
  
     return (
         <div className="App">
-            <header className="App-header">
-                <h1>React and flask</h1>
-                {/* Calling a data from setdata for showing */}
-                <p>{data.name}</p>
-                <p>{data.age}</p>
-                <p>{data.date}</p>
-                <p>{data.programming}</p>
- 
-            </header>
+            <div id='home' className='w-full h-screen text-center'>
+                <header className="App-header">
+                    <h1>React and flask</h1>
+                    {/* Calling a data from setdata for showing */}
+                    <p>{data.name}</p>
+                    <p>{data.age}</p>
+                    <p>{data.date}</p>
+                    <p>{data.programming}</p>
+    
+                </header>
+            </div>
         </div>
     );
 }
